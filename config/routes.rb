65be1261aaf0b58342cab52d90d7b99e3edddc1d
Ubuntu <ubuntu@ip-172-31-27-206.ap-northeast-1.compute.internal>
2020-07-31 15:46:04 +0000

    create all controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'answers/edit'
+  get 'questions/new'
+  get 'questions/index'
+  get 'questions/show'
+  get 'questions/edit'
+  get 'users/new'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
